21526d6e12df3282287ba24ce28db78578a589d9
samsany <1451691457@qq.com>
2020-05-27 16:52:40 +0800

    Mosidian后端项目框架

---
+++ mosidian-admin/src/main/java/io/mosidian/MosidianAdminApplication.java
@@ -0,0 +1,14 @@
+package io.mosidian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MosidianAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosidianAdminApplication.class, args);
+	}
+
+}
